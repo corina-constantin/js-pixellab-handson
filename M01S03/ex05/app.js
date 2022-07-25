8410d0c9fd94f27ddd54990af018828a356d88fb
corina-constantin <corina.constantin@zoho.com>
2022-07-25 12:55:26 +0300

    tema 20 iulie

--- M01S03/ex05/app.js
+++ M01S03/ex05/app.js
@@ -1,6 +1,6 @@
 var person = {
-  name: 'Corina',
-  surname: 'COnstantin',
+  name: 'Jane',
+  surname: 'Janis',
   age: 32,
   petOwner: true,
   skills: ['html', 'javascript', 'css'],
@@ -59,6 +59,58 @@ console.log(
 );
 
 console.warn(
-  `Afiseaza propozitia “Sunt xxx si acum invat JavaScript.” Nu folosi valoarea din arrayul de skills.`,
+  `Afiseaza propozitia: “Ma numesc xxx yyy si vreau sa invat javascript.” Foloseste notatia cu paranteze patrate.`,
 );
-console.log();
+console.log(
+  'Ma numesc ' +
+    person.name +
+    ' ' +
+    person.surname +
+    ' si vreau sa invat ' +
+    person.skills[1] +
+    '.',
+);
+
+console.warn(
+  `Afiseaza propozitia “Sunt xxx si stiu HTML si CSS.”. Nu folosi valoarea din arrayul de skills.`,
+);
+console.log(
+  'Sunt ' +
+    person.name +
+    ' si stiu ' +
+    person.skills[0].toUpperCase() +
+    ' si ' +
+    person.skills[2].toUpperCase() +
+    '.',
+);
+
+console.warn(
+  `Afiseaza propozitia “Am x prieteni: Larryson, Stevenson si Carolson.”. Foloseste proprietatea length si apeleaza direct proprietatile name ale obiectelor.`,
+);
+console.log(
+  'Am ' +
+    person.friends.length +
+    ' prieteni: ' +
+    person.friends[0].surname +
+    ', ' +
+    person.friends[1].surname +
+    ' si ' +
+    person.friends[2].surname +
+    '.',
+);
+
+console.warn(
+  `Afiseaza propozitia “Numele meu este xxx yyy, am zz ani si n prieteni.”. Foloseste metoda length pentru a calcula lungimea arrayului de prieteni.`,
+);
+console.log(
+  'Numele meu este ' +
+    person.name +
+    ' ' +
+    person.surname +
+    ',' +
+    ' am ' +
+    person.age +
+    ' ani si ' +
+    person.friends.length +
+    ' prieteni.',
+);
--- M01S03/ex07/app.js
+++ M01S03/ex07/app.js
@@ -1,6 +1,132 @@
-console.log(`
-    Dragos este instructor de programare. Dragos studiaza JavaScript de la 17 ani. 
-    Cand Dragos a absolvit facultatea si-a facut licenta in JavaScript. 
-    Dragos are si un master in tehnologii internet. 
-    Dragos este instructor Pixellab si preda JavaScript acolo.
-`);
+var person = {
+  firstName: 'Jane',
+  lastName: 'Janis',
+  email: 'x@y.com',
+  birthYear: 1995,
+  pets: [
+    {
+      name: 'Oli',
+      species: 'iepure',
+      age: 7,
+    },
+    {
+      name: 'Dory',
+      species: 'peste',
+      age: 2,
+    },
+    { name: 'Olaf', species: 'caine', age: 4 },
+  ],
+  zipCode: '1002',
+};
+
+console.warn(
+  'Afiseaza propozitia: “Numele meu este: xxx yyy si am x animale.”. Nu uita de proprietatea length a arrayului pets. ',
+);
+console.log(
+  'Numele meu este: ' +
+    person.firstName +
+    ' ' +
+    person.lastName +
+    ' si am ' +
+    person.pets.length +
+    ' animale' +
+    '.',
+);
+
+console.warn('Afiseaza propozitia: “Am acelasi email din copilarie: xxx.”.');
+console.log('Am acelasi email din copilarie: ' + person.email + '.');
+
+console.warn(
+  'Afiseaza propozitia: “Unul din cele x animale ale mele este species si are age ani.”',
+);
+console.log(
+  'Unul din cele ' +
+    person.pets.length +
+    ' animale ale mele este ' +
+    person.pets[1].species +
+    ' si are ' +
+    person.pets[1].age +
+    ' ani.',
+);
+
+console.warn(
+  'Calculeaza si afiseaza (folosind anul curent) anul de nastere al animalului de pe pozitia 2. ',
+);
+console.log((2022 - person.pets[2].age).toString());
+
+console.warn(
+  'Calculeaza si salveaza in variabila difference diferenta de ani dintre persoana si animalul de pe pozitia 0 si afiseaza aceasta diferenta. Foloseste anul curent. ',
+);
+
+var difference = 2022 - person.pets[0].age - person.birthYear;
+console.log(difference.toString());
+
+console.warn(
+  'Salveaza numele animalului de pe pozitia 0 intr-o variabila numita petName. ',
+);
+var petName = person.pets[0].name;
+console.log(petName);
+
+console.warn(
+  'Afiseaza propozitia: “Intre firstName si petName este o diferenta de difference ani.”.',
+);
+
+console.log(
+  `Intre ${person.firstName} si ${petName} este o diferenta de ${difference} ani.`,
+);
+
+console.warn(
+  'Afiseaza propozitia “ firstName, pet1, pet2, pet3 locuiesc toti in aceeasi casa” (folosind bracket notation pe arrayul pets)',
+);
+/*
+var li01 = document.getElementById('prop01');
+li01.innerText =
+  person.firstName +
+  ', ' +
+  person.pets[0].name +
+  ', ' +
+  person.pets[1].name +
+  ', ' +
+  person.pets[2].name +
+  ' locuiesc toti in aceeasi casa.';
+*/
+var li01 =
+  person.firstName +
+  ', ' +
+  person.pets[0].name +
+  ', ' +
+  person.pets[1].name +
+  ', ' +
+  person.pets[2].name +
+  ' locuiesc toti in aceeasi casa.';
+
+console.log(`${li01}`);
+document.getElementById('prop01').innerHTML = `${li01}`;
+
+console.warn(
+  'Calculeaza si afiseaza diferenta de varsta dintre animalul de pe pozitia 0 si cel de pe pozitia 2 ',
+);
+
+var li02 = document.getElementById('prop02');
+li02.innerText = person.pets[0].age - person.pets[2].age;
+
+console.warn(
+  'Afiseaza propozitia: “Ma numesc xxx yyy, m-am nascut in birthYear si codul meu postal este: zipCode”',
+);
+var li03 = document.getElementById('prop03');
+var name = person.firstName;
+var name2 = person.lastName;
+var birth = person.lastName;
+var code = person.zipCode;
+
+li03.innerText = `Ma numesc ${name} ${name2}, m-am nascut in ${birth} si codul meu postal este: ${code}.`;
+
+console.warn(
+  'Afiseaza propozitia: “Animalele mele s-au nascut in xxxx, xxx, respectiv xxx.” Foloseste anul curent pentru a efectua scaderea. ',
+);
+
+var li04 = document.getElementById('prop04');
+var year0 = 2022 - person.pets[0].age;
+var year1 = 2022 - person.pets[1].age;
+var year2 = 2022 - person.pets[2].age;
+li04.innerText = `Animalele mele s-au nascut in ${year0}, ${year1}, respectiv ${year2}.`;
