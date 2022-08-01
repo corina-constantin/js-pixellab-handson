df6e9632aa1b9ca36dfcac49721dca99a11bef22
corina-constantin <corina.constantin@zoho.com>
2022-08-01 22:01:58 +0300

    curs 1aug

---
+++ M01S05/ex02/app.js
@@ -0,0 +1,28 @@
+function calculateRectangleArea(width, height) {
+  return width * height;
+}
+
+console.warn(
+  `
+    Folosind functia calculateRectangleArea()
+    afiseaza suprafata unui dreptunghi de 4 pe 3
+  `,
+);
+console.log(calculateRectangleArea(4, 3));
+
+console.warn(
+  `
+    Afiseaza suprafata totala a unor dreptunghiuri
+    de 5 pe 3, respectiv 3 pe 2. Foloseste variabile.
+  `,
+);
+console.log(calculateRectangleArea(5, 3), calculateRectangleArea(3, 2));
+
+console.warn(
+  `
+    Calculeaza intr-o variabila numita
+    squareSurface aria unui patrat de 3 pe 3.
+  `,
+);
+const squareSurface = calculateRectangleArea(3, 3);
+console.log(squareSurface);
