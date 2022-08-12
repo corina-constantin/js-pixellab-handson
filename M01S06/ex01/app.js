a8f126b9e5c28dbb156ba9ce775c3e5ff4021b11
corina-constantin <corina.constantin@zoho.com>
2022-08-12 20:38:52 +0300

    tema 8august

--- M01S06/ex01/app.js
+++ M01S06/ex01/app.js
@@ -11,6 +11,8 @@ const car = {
   color: 'black',
   wheels: 4,
   speed: 50,
+  topSpeed: 160,
+  topReverseSpeed: -50,
   //flags:
   isTrunkOpen: false,
   areLightsOn: false,
@@ -46,4 +48,61 @@ const car = {
       self.turnLightsOff();
     }, 5000);
   },
+
+  stop: function () {
+    this.speed = 0;
+  },
+
+  setSpeed: function (mySpeed) {
+    if (mySpeed >= -50 && mySpeed <= 160) {
+      this.speed = mySpeed;
+    } else if (mySpeed < -50) {
+      this.speed = this.topReverseSpeed;
+    } else if (mySpeed > 160) {
+      this.speed = this.topSpeed;
+    }
+  },
 };
+
+console.warn(`
+Afiseaza propozitia: "Masina era marca make si se deplasa cu speed km/h.".
+`);
+
+console.log(`Masina era marca ${car.make} si se deplasa cu ${car.speed} km/h.`);
+
+console.warn(`
+Decelereaza masina cu 5 unitati apoi afiseaza propozitia: "Viteza noua este speed km/h".
+`);
+
+const decelerateCar = function () {
+  for (let i = 5; i > 0; i--) {
+    car.speed -= 1;
+  }
+  console.log(`Viteza noua este ${car.speed} km/h.`);
+};
+
+console.log(decelerateCar());
+
+console.warn(`
+Adauga o proprietate numita topSpeed si asigneaza-i valoarea 160.
+`);
+
+console.warn(`
+Adauga o proprietate numita topReverseSpeed si asigneaza-i valoarea -50.
+`);
+
+console.warn(`
+Adauga o metoda numita stop() care sa faca proprietatea speed 0, apoi afiseaza viteza.
+`);
+
+console.log(car.stop());
+console.log(`Viteza actuala este de ${car.speed} km/h.`);
+
+console.warn(`
+Adauga  o metoda numita setSpeed() care sa poata primi un parametru fix pentru
+ viteza. Metoda trebuie sa verifice ca nu se depasesc limitele inferioare si superioare,
+ caz in care se folosesc proprietatile topSpeed si topReverseSpeed
+`);
+
+console.log(car.setSpeed(23));
+console.log(`Viteza actuala este de ${car.speed} km/h.`);
--- M01S06/ex02/app.js
+++ M01S06/ex02/app.js
@@ -1,11 +1,27 @@
 const paragraphElement = document.createElement('p');
 const userAgentString = navigator.userAgent;
 let message = 'Nu stim ce browser folosesti.';
+let browser = '';
+const welcomeMessage = document.createElement('h1');
 
 if (userAgentString.includes('Chrome')) {
   message = 'Navighezi folosing Chrome';
+  browser = 'Chrome';
+} else if (userAgentString.includes('Firefox')) {
+  message = 'Navighezi folosind Firefox';
+  browser = 'Firefox';
 }
 
 paragraphElement.innerText = message;
 
 document.body.append(paragraphElement);
+document.body.append(welcomeMessage);
+
+setTimeout(() => {
+  const user = prompt(`${browser} vrea sa stie cum te cheama.`);
+  welcomeMessage.innerText = `Salut ${user}`;
+
+  setTimeout(() => {
+    paragraphElement.remove();
+  }, 3000);
+}, 3000);
