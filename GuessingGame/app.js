11c58d0115834c57d3abc03c62283c57cecfe767
corina-constantin <corina.constantin@zoho.com>
2022-08-26 18:38:42 +0300

    Guessing Game with max tries

--- GuessingGame/app.js
+++ GuessingGame/app.js
@@ -1,43 +1,41 @@
-const userInput = document.getElementById('userInput');
+let userInput = document.getElementById('userInput');
 const form = document.getElementById('form');
 const checkMessage = document.getElementById('checkMessage');
-const randomNumber = Math.floor(Math.random() * 100 + 1);
+let randomNumber = Math.floor(Math.random() * 100 + 1);
+let userTries = 1;
+const maxTries = 5;
 
 form.addEventListener('submit', function (event) {
   let userGuess = userInput.value;
   let message = '';
-  let userTries = 1;
 
-  for (userTries = 1; userTries < 6; userTries++) {
-    if (userGuess == randomNumber) {
-      //aici daca pun === nu imi valideaza
-      // console.log('Ai ghicit!');
-      message = 'Ai ghicit!';
-      checkButton.remove();
-      break;
-    } else if (userGuess > randomNumber) {
-      message = 'Ai introdus un numar prea mare.';
-      //   console.log('Ai introdus un numar prea mare.');
-    } else if (userGuess < randomNumber) {
-      message = 'Ai introdus un numar prea mic.';
-      //   console.log('Ai introdus un numar prea mic.');
-    }
-
-    if (userTries == 6) {
-      break;
-      checkButton.remove();
-    }
+  if (userTries === maxTries && userGuess !== randomNumber) {
+    message = 'Ai pierdut!';
+    checkButton.remove();
+  } else if (userGuess == randomNumber) {
+    message = 'Ai ghicit!';
+    checkButton.remove();
+  } else if (userGuess > randomNumber) {
+    message = 'Ai introdus un numar prea mare.';
+    userTries++;
+  } else if (userGuess < randomNumber) {
+    message = 'Ai introdus un numar prea mic.';
+    userTries++;
   }
 
   checkMessage.innerText = message;
   event.preventDefault();
 });
 
-/*
-Ar mai trebui:
-- validare pentru cazul in care numarul introdus e mai mare decat 1 si mai mic decat 100
-- daca nu a fost introdus niciun numar
-- sa se stearga numarul introdus dupa ce apas butonul de submit
-- sa se opreasca bucla cand trec de 5 incercari si sa dea un mesaj "Ai pierdut. Incearca din nou.". Momentan nu merge
-- ziceai ceva sa valideze numarul la Click, nu cand apas Enter?
+/* Incer o functie de reset
+
+form.addEventListener('reset', function (event) {
+  randomNumber = Math.floor(Math.random() * 100 + 1);
+  userTries = 1;
+  userGuess = '';
+  message = '';
+  document.getElementById('form').reset();
+  checkMessage.innerText = message;
+  event.preventDefault();
+});
 */
